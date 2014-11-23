f6b6fef23eea12a42fad3594729e53b2554ac663
Lidia <karrucha.1986@gmail.com>
2014-11-23 08:59:18 +0100

    1ª creación project para github

---
+++ src/main/java/edu/upc/ettac/dxat/lihect/WS/BBDD/App.java
@@ -0,0 +1,18 @@
+package edu.upc.ettac.dxat.lihect.WS.BBDD;
+
+
+
+/*
+ * Copyright [2014] [Lidia Ibern Ortega-Hector Ortiz Rua]*/
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
